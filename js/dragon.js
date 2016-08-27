916acba61f52d0bb1ea7d309ba39d58f599c45cd
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2016-08-27 17:01:27 -0400

    add buttons to dragon page

--- js/dragon.js
+++ js/dragon.js
@@ -1,1 +1,12 @@
-var $move
+var $village = $('#village');
+var $home = $('#home');
+
+
+
+$village.click(function() {
+  window.location.href='../html/village.html';
+});
+
+$home.click(function() {
+  window.location.href='../html/home.html';
+});
